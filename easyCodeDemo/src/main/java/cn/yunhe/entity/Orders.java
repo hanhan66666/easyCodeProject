fe29e7caf0320ff2600a03cb446305a40249d826
liuxiao <2578065229@qq.com>
2023-08-17 19:12:27 +0800

    提交easyCode项目

---
+++ easyCodeDemo/src/main/java/cn/yunhe/entity/Orders.java
@@ -0,0 +1,47 @@
+package com.sample;
+
+
+public class Orders {
+
+  private Integer id;
+  private Integer uid;
+  private String ordertime;
+  private double money;
+
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+
+  public Integer getUid() {
+    return uid;
+  }
+
+  public void setUid(Integer uid) {
+    this.uid = uid;
+  }
+
+
+  public String getOrdertime() {
+    return ordertime;
+  }
+
+  public void setOrdertime(String ordertime) {
+    this.ordertime = ordertime;
+  }
+
+
+  public double getMoney() {
+    return money;
+  }
+
+  public void setMoney(double money) {
+    this.money = money;
+  }
+
+}
