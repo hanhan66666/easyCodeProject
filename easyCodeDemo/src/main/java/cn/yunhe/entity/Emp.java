4bc75a283bb0206e009bdf8ef2ae42820a33a112
liuxiao <2578065229@qq.com>
2023-08-17 19:15:17 +0800

    提交easyCode项目

---
+++ easyCodeDemo/src/main/java/cn/yunhe/entity/Emp.java
@@ -0,0 +1,57 @@
+package com.sample;
+
+
+public class Emp {
+
+  private Integer empno;
+  private String ename;
+  private double salary;
+  private String hiredate;
+  private Integer deptno;
+
+
+  public Integer getEmpno() {
+    return empno;
+  }
+
+  public void setEmpno(Integer empno) {
+    this.empno = empno;
+  }
+
+
+  public String getEname() {
+    return ename;
+  }
+
+  public void setEname(String ename) {
+    this.ename = ename;
+  }
+
+
+  public double getSalary() {
+    return salary;
+  }
+
+  public void setSalary(double salary) {
+    this.salary = salary;
+  }
+
+
+  public String getHiredate() {
+    return hiredate;
+  }
+
+  public void setHiredate(String hiredate) {
+    this.hiredate = hiredate;
+  }
+
+
+  public Integer getDeptno() {
+    return deptno;
+  }
+
+  public void setDeptno(Integer deptno) {
+    this.deptno = deptno;
+  }
+
+}
